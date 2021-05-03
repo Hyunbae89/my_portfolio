6bf82ea5868f6d747cc2d6144b805afb959d3554
Hyunbae89 <zxcv6487@gmail.com>
2021-05-03 21:33:01 +0900

    install bootstrap && input Default Test Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
+import Component from "./components/Component";
+import _ from "lodash";
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  render(){
+    return(
+        <form>
+            <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
+                        else.</small>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
+            </div>
+            <div className="form-group form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    );
+  }
 }
+
 
 export default App;
